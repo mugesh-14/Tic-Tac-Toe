6e7d089b44bb7910386d33e5d6b50609440d63c1
Mugesh <mugesh1453@gmail.com>
2025-09-18 10:21:56 +0530

    update: add game board structure and styles

--- src/components/tic-tac-toe/ticTacToe.jsx
+++ src/components/tic-tac-toe/ticTacToe.jsx
@@ -8,7 +8,21 @@ const TicTacToe = () => {
     <div className='container'>
         <h1 className="title">Tic Tac Toe <span>React</span></h1>
         <div className="board">
-        
+          <div className="row1">
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+          </div>
+          <div className="row2">
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+          </div>
+          <div className="row3">
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+          </div>
         </div>
         <button className="reset">Reset</button> 
     </div>
