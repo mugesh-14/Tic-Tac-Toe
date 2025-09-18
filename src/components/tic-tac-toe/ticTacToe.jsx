318a97f9821c3c095fc2f4152e511e5cd082c569
Mugesh <mugesh1453@gmail.com>
2025-09-18 10:13:02 +0530

    add: initial CSS styles for heading and button

--- src/components/tic-tac-toe/ticTacToe.jsx
+++ src/components/tic-tac-toe/ticTacToe.jsx
@@ -8,7 +8,7 @@ const TicTacToe = () => {
     <div className='container'>
         <h1 className="title">Tic Tac Toe <span>React</span></h1>
         <div className="board">
-
+        
         </div>
         <button className="reset">Reset</button> 
     </div>
